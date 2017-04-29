dfd3d0bcc0493b77fd80d9b9ea89edf9bdfcf0b2
Jonathan Romualdo <jrsap@a.recife.ifpe.edu.br>
2017-04-28 23:42:57 -0300

    adsgfsadfdsaf

---
+++ Projeto/sistemaPetShop/target/generated-sources/annotations/com/sistema/model/Funcionario_.java
@@ -0,0 +1,16 @@
+package com.sistema.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Funcionario.class)
+public abstract class Funcionario_ extends com.sistema.model.Usuario_ {
+
+	public static volatile SingularAttribute<Funcionario, EspecialidadeFuncionario> especialidadeFuncionario;
+	public static volatile ListAttribute<Funcionario, ConsultaGeral> listaConsultaGeral;
+
+}
+
