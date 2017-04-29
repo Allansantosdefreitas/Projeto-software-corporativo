93d3c8c7710f93b74dfa348ee6f084d66450b664
Jonathan Romualdo <jrsap@a.recife.ifpe.edu.br>
2017-04-29 00:00:12 -0300

    Classes de testes criadas.

---
+++ Projeto/sistemaPetShop/src/test/java/sistema/testes/ConsultaGeralTest.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.testes;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ConsultaGeralTest {
+
+    private static EntityManagerFactory emf;
+    private static Logger logger;
+    private EntityManager em;
+    private EntityTransaction et;
+
+    public ConsultaGeralTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        logger = Logger.getGlobal();
+        logger.setLevel(Level.INFO);
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        emf.close();
+    }
+
+    @Before
+    public void setUp() {
+        emf = Persistence.createEntityManagerFactory("exemplo_12");
+        DbUnitUtil.inserirDados();
+
+        em = emf.createEntityManager();
+        et = em.getTransaction();
+        et.begin();
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            et.commit();
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, ex.getMessage());
+
+            if (et.isActive()) {
+                et.rollback();
+            }
+        } finally {
+            em.close();
+            em = null;
+            et = null;
+        }
+    }
+
+}
